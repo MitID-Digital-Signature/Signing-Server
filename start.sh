9962da1e3b11240cf549625e1b7a9ccf3d7d4094
root <root@nemsignering.bellcom.dk>
2025-09-25 08:58:35 +0000

    Added target folders to gitignore. Changed the script to automatically cd into the folder where it is placed

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Always run from the folder where start.sh is located
+cd "$(dirname "$0")" || exit 1
 
 umask 002
-cd /var/www/signering
 java -jar examples/nemlogin-signing-webapp/target/nemlogin-signing-webapp-1.0.17-exe.jar
